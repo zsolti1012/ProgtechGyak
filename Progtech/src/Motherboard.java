811b287dea287043cb20dabeb28553a7d79e6c3f
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2020-05-01 18:12:06 +0200

    Update Motherboard.java

--- Progtech/src/Motherboard.java
+++ Progtech/src/Motherboard.java
@@ -1,3 +1,11 @@
+import Commom.Camera;
+import Commom.Battery;
+import Commom.Memory;
+import Commom.Microphone;
+import Commom.Processor;
+import Commom.Screen;
+import Commom.Speaker;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +17,20 @@
  *
  * @author O.Zsolt
  */
-public class Motherboard {
+public interface Motherboard {
     
+    
+    
+    public   Sockets supportedMemory();
+    public  Sockets supportedCPU();
+    public double sataissupported();
+    public double usb3issupported();
+    
+    public Battery battery();
+    public Camera camera();
+    public Memory memory();
+    public Microphone microphone();
+    public Processor processor();
+    public Screen screen();
+    public Speaker speaker();
 }
