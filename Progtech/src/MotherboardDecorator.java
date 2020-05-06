2592acf2087c237526ec7723273bf6a923fde7fc
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2020-05-06 15:57:33 +0200

    Update MotherboardDecorator.java

--- Progtech/src/MotherboardDecorator.java
+++ Progtech/src/MotherboardDecorator.java
@@ -8,6 +8,62 @@
  *
  * @author O.Zsolt
  */
-public class MotherboardDecorator {
+public class MotherboardDecorator extends Motherboard{
+
+    private Motherboard _motherboard;
     
+    @Override
+    public String battery() {
+        return _motherboard.battery();
+    }
+
+    @Override
+    public String camera() {
+        return _motherboard.camera();
+    }
+
+    @Override
+    public String memory() {
+        return _motherboard.memory();
+    }
+
+    @Override
+    public String microphone() {
+        return _motherboard.microphone();
+    }
+
+    @Override
+    public String processor() {
+        return _motherboard.processor();
+    }
+
+    @Override
+    public String screen() {
+     return _motherboard.screen();
+    }
+
+    @Override
+    public String speaker() {
+        return _motherboard.speaker();
+    }
+
+    @Override
+    public String touchpanel() {
+        return _motherboard.touchpanel();
+    }
+
+    @Override
+    public String keyboard() {
+     return _motherboard.keyboard();
+    }
+
+    @Override
+    public String touchpad() {
+     return _motherboard.touchpad();
+    }
+
+    @Override
+    public String wrap() {
+    return _motherboard.wrap();
+    }
 }
